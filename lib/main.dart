37d403cac8d2ff025c87204e835832618edd4307
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-02-27 14:35:40 +0700

    commit: Setup main.dart dengan Provider dan Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'cart_model.dart';
+// Nanti kita bikin dua file ini di langkah selanjutnya:
+import 'catalog_page.dart';
+import 'cart_page.dart';
 
+void main() {
+  runApp(
+    // Membungkus aplikasi dengan ChangeNotifierProvider
+    // agar State bisa diakses di mana saja
+    ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+// 2. UI LAYER
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      // Mengatur rute/navigasi pindah halaman
+      routes: {
+        '/': (context) => const MyCatalog(),
+        '/cart': (context) => const MyCart(),
+      },
+    );
   }
 }
